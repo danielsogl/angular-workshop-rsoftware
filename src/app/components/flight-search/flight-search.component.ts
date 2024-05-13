2ad0f861b2e4d040634396842d8d36ae835ea7fa
Daniel Sogl <me@danielsogl.de>
2024-05-13 15:29:44 +0200

    add exercise 1 solution

--- src/app/components/flight-search/flight-search.component.ts
+++ src/app/components/flight-search/flight-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { Flight } from '../../entities/flight';
 import { HttpClient } from '@angular/common/http';
 
@@ -9,14 +9,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FlightSearchComponent {
   from = 'London';
-  to = 'Paris';
+  to = 'Wien';
 
   flights: Flight[] = [];
+  selectedFlight: Flight | undefined;
+  message = '';
 
   constructor(private http: HttpClient) { }
 
   search(): void {
-    const url = 'https://demo.angulararchitects.io/api/flights';
+    const url = 'https://demo.angulararchitects.io/api/flight';
     const params = { from: this.from, to: this.to };
     const headers = { Accept: 'application/json' };
 
@@ -27,9 +29,30 @@ export class FlightSearchComponent {
       error: err => {
         console.error('err', err);
       },
-      complete: () => {
-        console.debug('complete');
-      }
     });
+  }
+
+  selectFlight(f: Flight): void {
+    this.selectedFlight = { ...f };
+  }
+
+  save(): void {
+    if (!this.selectedFlight) return;
+
+    const url = 'https://demo.angulararchitects.io/api/flight';
+    const headers = { Accept: 'application/json' };
+
+    this.http
+      .post<Flight>(url, this.selectedFlight, { headers })
+      .subscribe({
+        next: flight => {
+          this.selectedFlight = flight;
+          this.message = 'Update successful!';
+        },
+        error: err => {
+          this.message = 'Error on updating the Flight';
+          console.error(this.message, err);
+        },
+      });
   }
 }
