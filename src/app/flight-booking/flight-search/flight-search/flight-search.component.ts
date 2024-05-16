b485a7f9d9b7e21f62be781ea77fdfba0d50e213
Daniel Sogl <me@danielsogl.de>
2024-05-16 14:51:42 +0200

    add eslint, prettier and husky

--- src/app/flight-booking/flight-search/flight-search/flight-search.component.ts
+++ src/app/flight-booking/flight-search/flight-search/flight-search.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FlightService } from '../flight.service';
 import { DefaultFlightService } from '../default-flight.service';
 import { Flight } from '../../../entities/flight';
-import { Form, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CityValidators } from '../../../shared/validators/city.validator';
 import { FlightSearchFacadeService } from '../flight-search-facade.service';
 
@@ -16,9 +16,7 @@ export interface SearchForm {
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
   styleUrl: './flight-search.component.scss',
-  providers: [
-    { provide: FlightService, useClass: DefaultFlightService }
-  ],
+  providers: [{ provide: FlightService, useClass: DefaultFlightService }],
 })
 export class FlightSearchComponent implements OnInit {
   // @ViewChild('form', { static: true }) form!: NgForm;
@@ -26,9 +24,11 @@ export class FlightSearchComponent implements OnInit {
   private readonly validators = [
     Validators.required,
     Validators.minLength(3),
-    CityValidators.validateCity(['Graz', 'Hamburg', 'Wien', 'Zürich', 'Foo'])
+    CityValidators.validateCity(['Graz', 'Hamburg', 'Wien', 'Zürich', 'Foo']),
   ];
-  private readonly asyncValidators = [CityValidators.asyncValidateCity(this.flightService)];
+  private readonly asyncValidators = [
+    CityValidators.asyncValidateCity(this.flightService),
+  ];
 
   from = 'London';
   to = 'Wien';
@@ -41,36 +41,43 @@ export class FlightSearchComponent implements OnInit {
 
   basket: Record<number, boolean> = {};
 
-  form = new FormGroup<SearchForm>({
-    from: new FormControl('London', {
-      validators: this.validators,
-      asyncValidators: this.asyncValidators,
-      nonNullable: true,
-    }),
-    to: new FormControl('Wien', {
-      validators: this.validators,
-      asyncValidators: this.asyncValidators,
-      nonNullable: true,
-    }),
-    withValidators: new FormControl(true, { nonNullable: true }),
-  }, {
-    validators: [CityValidators.roundTrip]
-  });
+  form = new FormGroup<SearchForm>(
+    {
+      from: new FormControl('London', {
+        validators: this.validators,
+        asyncValidators: this.asyncValidators,
+        nonNullable: true,
+      }),
+      to: new FormControl('Wien', {
+        validators: this.validators,
+        asyncValidators: this.asyncValidators,
+        nonNullable: true,
+      }),
+      withValidators: new FormControl(true, { nonNullable: true }),
+    },
+    {
+      validators: [CityValidators.roundTrip],
+    }
+  );
 
-  constructor(protected facade: FlightSearchFacadeService, protected flightService: FlightService) { }
+  constructor(
+    protected facade: FlightSearchFacadeService,
+    protected flightService: FlightService
+  ) {}
 
   ngOnInit(): void {
-    this.form.controls.withValidators.valueChanges.subscribe((withValidators) => {
-      ;
-      if (withValidators) {
-        this.form.controls.from.setValidators(this.validators);
-        this.form.controls.to.setValidators(this.validators);
-      } else {
-        this.form.controls.from.clearValidators();
-        this.form.controls.to.clearValidators();
+    this.form.controls.withValidators.valueChanges.subscribe(
+      (withValidators) => {
+        if (withValidators) {
+          this.form.controls.from.setValidators(this.validators);
+          this.form.controls.to.setValidators(this.validators);
+        } else {
+          this.form.controls.from.clearValidators();
+          this.form.controls.to.clearValidators();
+        }
+        this.form.updateValueAndValidity();
       }
-      this.form.updateValueAndValidity();
-    });
+    );
   }
 
   addDummyData(): void {
